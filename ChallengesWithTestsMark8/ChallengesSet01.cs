93c1f949148b67d70d9dbb955dc982987978dbcb
David Chuvik <dchuvik@gmail.com>
2022-06-09 18:54:55 -0400

    Did ChallengeSet01

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -3,8 +3,8 @@
 namespace ChallengesWithTestsMark8
 {
     public class ChallengesSet01
-    {
-        public bool AreTwoNumbersTheSame(int num1, int num2)
+    {       
+        public bool AreTwoNumbersTheSame(int num1, int num2) //done
         {
             if(num1 == num2)
             {
@@ -16,33 +16,50 @@ namespace ChallengesWithTestsMark8
             }
         }
 
-        public double Subtract(double minuend, double subtrahend)
+        public double Subtract(double minuend, double subtrahend) //done
         {
+            return minuend - subtrahend;
             throw new NotImplementedException();
         }
 
-        public int Add(int number1, int number2)
+        public int Add(int number1, int number2)//done
         {
+            return number1 + number2;
             throw new NotImplementedException();
         }
 
-        public int GetSmallestNumber(int number1, int number2)
+        public int GetSmallestNumber(int number1, int number2) //done
         {
+            if (number1 > number2)
+            {
+                return number2;
+            }
+            else return number1;
             throw new NotImplementedException();
         }
 
         public long Multiply(long factor1, long factor2)
         {
+            return factor1 * factor2;
             throw new NotImplementedException();
         }
 
         public string GetGreeting(string nameOfPerson)
         {
+            if (nameOfPerson == "")
+            {
+                return $"Hello!";
+            }
+            else
+            {
+                return $"Hello, {nameOfPerson}!";
+            }           
             throw new NotImplementedException();
         }
 
         public string GetHey()
         {
+            return "HEY!";
             throw new NotImplementedException();
         }
     }
--- ChallengesWithTestsMark8/ChallengesSet02.cs
+++ ChallengesWithTestsMark8/ChallengesSet02.cs
@@ -5,28 +5,63 @@ namespace ChallengesWithTestsMark8
 {
     public class ChallengesSet02
     {
-        public bool CharacterIsALetter(char c)
+        public bool CharacterIsALetter(char c) // done
         {
+            if (Char.IsLetter(c))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
             throw new NotImplementedException();
         }
 
-        public bool CountOfElementsIsEven(string[] vals)
+        public bool CountOfElementsIsEven(string[] vals) //done
         {
+                if (vals.Length % 2 == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+        
             throw new NotImplementedException();
         }
 
-        public bool IsNumberEven(int number)
+        public bool IsNumberEven(int number) //done
         {
+            if (number % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
             throw new NotImplementedException();
         }
 
-        public bool IsNumberOdd(int num)
+        public bool IsNumberOdd(int num) //done
         {
+            if (num % 2 != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            throw new NotImplementedException();
             throw new NotImplementedException();
         }
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
         {
+            double.Max(numbers)
             throw new NotImplementedException();
         }
 
