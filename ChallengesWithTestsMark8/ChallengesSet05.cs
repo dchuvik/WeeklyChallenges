c9b846e434213e5a5b21375b0e2e4898b74a37ae
David Chuvik <dchuvik@gmail.com>
2022-07-15 16:05:50 -0400

    challenge set 5

--- ChallengesWithTestsMark8/ChallengesSet05.cs
+++ ChallengesWithTestsMark8/ChallengesSet05.cs
@@ -1,43 +1,129 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
     public class ChallengesSet05
     {
-        public int GetNextNumberDivisibleByN(int startNumber, int n)
+        public int GetNextNumberDivisibleByN(int startNumber, int n) //done
         {
-            throw new NotImplementedException();
+            //var i = startNumber;
+            //do
+            //{
+            //    i++;
+            //}
+            //while (i % n != 0);
+            //return i;        
+
+            return ((startNumber / n) + 1) * n;
         }
 
-        public void ChangeNamesOfBusinessesWithNoRevenueTo_CLOSED(Business[] businesses)
+        public void ChangeNamesOfBusinessesWithNoRevenueTo_CLOSED(Business[] businesses) //Done
         {
-            throw new NotImplementedException();
+            foreach (var business in businesses)
+            {
+                if (business.TotalRevenue == 0)
+                {
+                    business.Name = "CLOSED";
+                }
+            }
         }
 
-        public bool IsAscendingOrder(int[] numbers)
+        public bool IsAscendingOrder(int[] numbers) //done
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Length == 0)
+            {
+                return false;
+            }
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] < numbers[i - 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+
         }
 
-        public int SumElementsThatFollowAnEven(int[] numbers)
+        public int SumElementsThatFollowAnEven(int[] numbers) // Done
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Length == 0)
+            {
+                return 0;
+            }
+            var sum = 0;
+            for (int n = 1; n < numbers.Length; n++)
+            {
+                if (numbers[n - 1] % 2 == 0)
+                {
+                    sum += numbers[n];
+                }
+            }
+            return sum;
         }
 
-        public string TurnWordsIntoSentence(string[] words)
+        public string TurnWordsIntoSentence(string[] words) //Done
         {
-            throw new NotImplementedException();
+            var sentence = "";
+            if (words == null || words.Length == 0)
+            {
+                return "";
+            }
+
+            foreach (var word in words)
+            {
+                if (word.Trim().Length > 0)
+                {
+                    sentence += word.Trim() + " ";
+                }
+            }
+
+            if (sentence.Length == 0)
+            {
+                return "";
+            }
+
+            sentence = sentence.Trim();
+            sentence += ".";
+            return sentence;
+
         }
 
-        public double[] GetEveryFourthElement(List<double> elements)
+        public double[] GetEveryFourthElement(List<double> elements) //Done
         {
-            throw new NotImplementedException();
+            List<double> result = new List<double>();
+
+            if (elements == null || elements.Count == 0)
+            {
+                return result.ToArray();
+            }
+
+            foreach (var element in elements)
+            {
+                if (element % 4 == 0)
+                {
+                    result.Add(element);
+                }
+            }
+            return result.ToArray();
         }
 
         public bool TwoDifferentElementsInArrayCanSumToTargetNumber(int[] nums, int targetNumber)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == targetNumber)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
         }
     }
 }
